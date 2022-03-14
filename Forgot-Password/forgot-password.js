9120a391b2bdd744e9b68de62d23ba3e2b97aa5c
Jack-Crowley <jcrowley.jjc@gmail.com>
2022-03-14 14:55:33 -0400

    Added cookies

--- Forgot-Password/forgot-password.js
+++ Forgot-Password/forgot-password.js
@@ -1,9 +1,25 @@
-window.location.replace("https://jaccro25.github.io/The-Lost-Recipe/Main-Page/recipe.html");
+function hash(message) {
+    let hash = 0
+    for (let len = 0; len < message.length; len++) {
+        hash *= 58
+        hash += message.charCodeAt(len)
+    }
+    return hash
+}
 
 let x = (document.cookie).split('; ')
 let cookiemap = new Map()
 for (let i = 0; i < x.length; i++) {
     cookiemap.set(x[i].split('=')[0], x[i].split('=')[1])
+}
+
+let cookieHash = 0
+let useragentHash = 0
+let langHash = 0
+
+if (!(hash(cookiemap.get('Username')) == cookieHash)) {
+    window.location.replace("https://jaccro25.github.io/The-Lost-Recipe/Main-Page/recipe.html");
+    alert('ID is not correct; access denied')
 }
 
 let cookie = document.getElementById("cookie");
